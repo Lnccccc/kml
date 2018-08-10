afa386f944e37d7245a857fdfa70d7d2a9ca5d8e
lnc <601452185@qq.com>
2018-08-10 08:26:44 +0800

    Class rewrite

--- get_kml.py
+++ get_kml.py
@@ -1,31 +1,40 @@
 from bs4 import BeautifulSoup
 import sys
 import xlwt
+from progressbar import *
+class GetKml:
+    def __init__(self):
+        self.progress = ProgressBar()
+        self.gpus = sys.argv[1]
+        self.name_list = []
+        self.lat_lng = []
+        self.lat_lng_2 = []
+        self. lat_lng_3 = []
+    def Get_kml_raw(self):
+        soup = BeautifulSoup(open(str(self.gpus),'rb'),"xml")
+        for i in soup.Folder.find_all('Placemark'):
+            self.name_list.append(i.contents[1].get_text()) ##获取区块名称
+            self.lat_lng.append(i.coordinates.get_text().strip()) ##获取区块经纬度
+        for i in self.lat_lng:
+            self.lat_lng_2.append(i.split(' '))
+        for i in range(len(self.lat_lng_2)):
+            for j in range(len(self.lat_lng_2[i])):
+                tmp = self.name_list[i]+','+self.lat_lng_2[i][j]
+                self.lat_lng_3.append(tmp)
+    def WriteExcel(self):
+        wb = xlwt.Workbook('utf-8')
+        sh = wb.add_sheet('kml')
+        sh.write(0,0,'name')
+        sh.write(0,1,'lng')
+        sh.write(0,2,'lat')
+        for i in self.progress(range(len(self.lat_lng_3))):
+            sh.write(i+1,0,self.lat_lng_3[i].split(',')[0])
+            sh.write(i+1,1,self.lat_lng_3[i].split(',')[1])
+            sh.write(i+1,2,self.lat_lng_3[i].split(',')[2])
+            wb.save('kml.xls')
 
-gpus = sys.argv[1]
-name_list = []
-lat_lng = []
-lat_lng_2 = []
-lat_lng_3 = []
-soup = BeautifulSoup(open(str(gpus),'rb'),"xml")
-for i in soup.Folder.find_all('Placemark'):
-    name_list.append(i.contents[1].get_text()) ##获取区块名称
-    lat_lng.append(i.coordinates.get_text().strip()) ##获取区块经纬度
-for i in lat_lng:
-    lat_lng_2.append(i.split(' '))
-
-wb = xlwt.Workbook('utf-8')
-sh = wb.add_sheet('kml')
-sh.write(0,0,'name')
-sh.write(0,1,'lng')
-sh.write(0,2,'lat')
-for i in range(len(lat_lng_2)):
-    for j in range(len(lat_lng_2[i])):
-        tmp = name_list[i]+','+lat_lng_2[i][j]
-        lat_lng_3.append(tmp)
-print(lat_lng_3)
-for i in range(len(lat_lng_3)):
-    sh.write(i+1,0,lat_lng_3[i].split(',')[0])
-    sh.write(i+1,1,lat_lng_3[i].split(',')[1])
-    sh.write(i+1,2,lat_lng_3[i].split(',')[2])
-wb.save('kml.xls')
+if __name__ == '__main__':
+    kml = GetKml()
+    kml.Get_kml_raw()
+    kml.WriteExcel()
+    print('Down')
